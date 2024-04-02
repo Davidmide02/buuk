b1b315e7e30fb7b879c3b9f0c06378e2cc6c2c92
Davidmide <Davidmide07@gmail.com>
2024-04-02 17:21:47 +0000

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import { AntdRegistry } from "@ant-design/nextjs-registry";
 import AppHeader from "../components/header";
 import { ConfigProvider } from "antd";
 import { Breadcrumb, Layout, } from "antd";
+import Footer from "@/components/footer";
 const { Header } = Layout;
 
 const inter = Inter({ subsets: ["latin"] });
@@ -37,6 +38,7 @@ export default function RootLayout({
               <AppHeader />
         
             {children}
+            <Footer/>
           </ConfigProvider>
         </AntdRegistry>
       </body>
---
+++ src/components/footer.tsx
@@ -0,0 +1,103 @@
+"use client";
+import React from "react";
+// import footerImage from "@/as"
+import footerImage from "@/assest/footer.png";
+import {
+  TwitterOutlined,
+  GithubOutlined,
+  LinkedinOutlined,
+} from "@ant-design/icons";
+import Link from "next/link";
+// import { Button, Form, Input } from "antd";
+import { Button, Checkbox, Form, type FormProps, Input } from "antd";
+
+type FieldType = {
+  username?: string;
+  password?: string;
+  remember?: string;
+};
+
+const Footer = () => {
+  const onFinish: FormProps<FieldType>["onFinish"] = (values) => {
+    console.log("Success:", values);
+  };
+
+  const onFinishFailed: FormProps<FieldType>["onFinishFailed"] = (
+    errorInfo
+  ) => {
+    console.log("Failed:", errorInfo);
+  };
+
+  return (
+    <footer
+      style={{ backgroundImage: `url(${footerImage})` }}
+      className=" bg-primary text-white"
+    >
+      <div className="con grid grid-cols-4 justify-between p-8">
+        <div className="socials">
+          <Link
+            href={"https://twitter.com/Davidmide02"}
+            className="text-2xl hover:text-gray-400 p-1"
+          >
+            <TwitterOutlined />
+          </Link>
+          <Link
+            href={"https://github.com/Davidmide02"}
+            className="text-2xl hover:text-gray-400 p-1"
+          >
+            <GithubOutlined />
+          </Link>
+          <Link
+            href={"https://linkedin.com/david-adenusi-27davidmide/"}
+            className="text-2xl hover:text-gray-400 p-1"
+          >
+            <LinkedinOutlined />
+          </Link>
+        </div>
+
+        <div className="newsletter col-span-2">
+          <Form
+            name="basic"
+            labelCol={{ span: 8 }}
+            wrapperCol={{ span: 16 }}
+            style={{ maxWidth: 600 }}
+            initialValues={{ remember: true }}
+            onFinish={onFinish}
+            onFinishFailed={onFinishFailed}
+            autoComplete="off"
+          >
+            <Form.Item<FieldType>
+              label="Email"
+              name="username"
+              rules={[
+                { required: true, message: "Please input your email!" },
+                {
+                  type: "email",
+                  message: "Please enter a valid email address!",
+                },
+              ]}
+              style={{ color: "white" }}
+            >
+              <Input placeholder="example101@gmail.com" />
+            </Form.Item>
+
+            <Form.Item wrapperCol={{ offset: 8, span: 16 }}>
+              <Button htmlType="submit">Subscribe</Button>
+            </Form.Item>
+          </Form>
+        </div>
+
+        <div className="dev-info">
+          <Link
+            href={"https://github.com/Davidmide02"}
+            className="hover:text-gray-400 p-1"
+          >
+            <h3>Develop and Design by Davidmide ❤</h3>
+          </Link>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
